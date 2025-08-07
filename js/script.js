43363124d975e238965e61fa7839c711e406bd9e
Brendan Sheedy <60196992+BLSHEE22@users.noreply.github.com>
2025-08-07 13:57:09 -0400

    script test 6

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const thursdayHeader = document.getElementById("thursdayHeader");
-thursdayHeader.textContext = "Thursday, September 4th - BOOYAH";
+const thursdayHeader = document.getElementById('thursdayHeader');
+thursdayHeader.textContext = 'Thursday, September 4th - BOOYAH';
 thursdayHeader.addEventListener('click', () => {
         alert('Paragraph clicked!');
     });
-newP = document.createElement("p");
-newP.style.color = "tomato";
-newP.textContext = "THIS IS A JS TEST";
+newP = document.createElement('p');
+newP.style.color = 'tomato';
+newP.textContext = 'THIS IS A JS TEST';
 document.getElementById("centered-div").append(newP);
