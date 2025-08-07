8a3aefb178930a289d94ef9f1795382ade1d794c
Brendan Sheedy <60196992+BLSHEE22@users.noreply.github.com>
2025-08-07 14:26:23 -0400

    test adding content vis script 2

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ thursdayHeader.innerHTML = "BOOYAHHHH";
 thursdayHeader.addEventListener('click', () => {
         alert('Paragraph clicked!');
     });
-newP = document.createElement('p');
-newP.style.color = 'tomato';
-newP.innerHTML = 'THIS IS A JS TEST';
-document.getElementById('thursdayHeader').append(newP);
+
+const parentElement = document.getElementById('myDiv');
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'This is new content.';
+parentElement.appendChild(newParagraph);
