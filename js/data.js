2df073beec3b18ff54d977b62d27b61e185a18bb
Brendan Sheedy <60196992+BLSHEE22@users.noreply.github.com>
2025-08-12 02:05:02 -0400

    reformatted data.js

--- js/data.js
+++ js/data.js
@@ -1,18 +1,18 @@
 // put giant dictionary here!
 export const weekByWeekInfo = {1:{'thursday':{'date':'Thursday, September 4th',
-                                                'matchups':[{'header':'Dallas Cowboys @ Philadelphia Eagles',
-                                                             'divisional': true,
-                                                             'homeGrudges':[{'name':'Miles Sanders',
-                                                                             'position':'RB',
-                                                                             'grudgeType':'Primary Grudge',                                                  
-                                                                             'seasons':'2019-2022',
-                                                                             'positionRk':'59'},
-                                                                            {'name':'Parris Campbell',
-                                                                             'grudgeType':'Grudge',   
-                                                                             'seasons': '2024',                                                                
-                                                                             'positionRk':'153'}]',
-                                                             'awayGrudges':['None'],
-                                                           }]
+                                              'matchups':[{'header':'Dallas Cowboys @ Philadelphia Eagles',
+                                                           'divisional': true,
+                                                           'awayGrudges':[{'name':'Miles Sanders',
+                                                                           'position':'RB',
+                                                                           'grudgeType':'Primary Grudge',                                                  
+                                                                           'seasons':'2019-2022',
+                                                                           'positionRk':'59'},
+                                                                          {'name':'Parris Campbell',
+                                                                           'grudgeType':'Grudge',   
+                                                                           'seasons': '2024',                                                                
+                                                                           'positionRk':'153'}]',
+                                                           'homeGrudges':['None'],
+                                                         }]
                                               }
-                                }
-                              };
+                                  }
+                               };
