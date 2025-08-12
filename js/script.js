86ae9e28351e81a62eee45bb1eb8900f25b27da9
Brendan Sheedy <60196992+BLSHEE22@users.noreply.github.com>
2025-08-12 02:57:27 -0400

    test week logic 20

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@
 // }
 
 
-const week = 1;
+let week = 1;
 const days = ['thursday', 'friday', 'saturday', 'sunday', 'monday'];
 const games = [1, 2, 3];
 const awayGrudges = ['a', 'b'];
@@ -42,14 +42,14 @@ const startDate = new Date('2025-08-01');
 const endDate = new Date('2025-09-02');
 const now = new Date();
 
-const weekObj = document.getElementById('what-week-is-it');
+let weekObj = document.getElementById('what-week-is-it');
 
 if (now >= startDate && now <= endDate) {
     weekObj.innerHTML = `
-  <h1>Welcome, ${week}!</h1>
-  <p>Your total is: 100</p>
-  <button>Buy Now</button>
-`;
+      <h1>Welcome, ${week}!</h1>
+      <p>Your total is: 100</p>
+      <button>Buy Now</button>
+    `;
     //weekObj.innerHTML = `<p>Yes, there are <strong>several</strong> taking place in week ${week}.</p>`;
     // document.getElementById('what-week-is-it').innerHTML = `<p>Yes, there are <strong>${awayGrudges.length + homeGrudges.length}</strong> grudge matches taking place in <a href=#upcoming-week>${week}</a>.</p>`;
 }
