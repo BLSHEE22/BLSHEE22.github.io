01850aa018f1512d48d777e309346ed6f684cc74
Brendan Sheedy <60196992+BLSHEE22@users.noreply.github.com>
2025-08-12 22:12:10 -0400

    removed extra breaks

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ let weekObj = document.getElementById('what-week-is-it');
 if (now >= startDate && now <= endDate) {
     weekObj.innerHTML = `
       <p>Yes, there are <strong>${awayGrudges.length}</strong> grudge matches taking place in <a href=#upcoming-week> week ${week}</a>.</p>
-      <br><br><br><br><hr style="background-color: solidgray;">
+      <br><br><hr style="background-color: solidgray;">
       <h2 id="upcoming-week">Week 1</h2>
     `;
 }
