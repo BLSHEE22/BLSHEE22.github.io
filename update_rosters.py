33f9ed41d39e0869e1a0c106098aece21030b807
Brendan Sheedy <60196992+BLSHEE22@users.noreply.github.com>
2025-08-28 06:39:29 -0400

    update full list of teams in nightly job, set cron job

--- update_rosters.py
+++ update_rosters.py
@@ -414,7 +414,7 @@ if __name__ == "__main__":
     ssl_context.check_hostname = True
     ssl_context.verify_mode = ssl.CERT_REQUIRED
     print("SSL established.")
-    for team in list(nflTeamTranslator.values())[:8]:
+    for team in list(nflTeamTranslator.values()):
         print(f">>> Getting latest {team} roster...")
         try:
             roster = Roster(team, conn)
